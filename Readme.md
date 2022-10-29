e9f9d5e54c68a4e08d741399b1b97de2de7739dc
MosesTheSweed <MosesTheSweed@proton.me>
2022-10-29 11:56:09 -0400

    Adding Sidebar

--- Readme.md
+++ Readme.md
@@ -1,10 +1,9 @@
-
-
-
+#### Built based on [this video](https://www.youtube.com/watch?v=wYpCWwD1oz0) from this [repo](https://github.com/ed-roh/react-admin-dashboard)
+##### Left off at about 1:27 mark
 
 ## Vite app
 `yarn create vite`
 
 ### Packages initially installed
-`yarn add @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo prop-types
+`yarn add @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar@0.7.1 formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo prop-types
 `
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import {ColorModeContext, useMode} from '/src/theme.js';
 import {CssBaseline, ThemeProvider} from '@mui/material';
+import {Routes, Route} from 'react-router-dom';
 import Topbar from '/src/scenes/global/Tobbar.jsx';
+import Sidebar from '/src/scenes/global/Sidebar.jsx';
+// import Dashboard from '/src/scenes/dashboard.jsx';
+// import Team from '/src/scenes/team.jsx';
+// import Invoices from '/src/scenes/invoices.jsx';
+// import Contacts from '/src/scenes/contacts.jsx';
+// import Bar from '/src/scenes/bar.jsx';
+// import Form from '/src/scenes/form.jsx';
+// import Line from '/src/scenes/line.jsx';
+// import Pie from '/src/scenes/pie.jsx';
+// import FAQ from '/src/scenes/faw.jsx';
+// import Geography from '/src/scenes/geography.jsx';
+// import Calendar from '/src/scenes/calendar.jsx';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +23,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className='content'>
             <Topbar />
+            <Routes>
+              {/*<Route path='/' element={<Dashboard />} />*/}
+              {/*<Route path='/team' element={<Team />} />*/}
+              {/*<Route path='/contacts' element={<Contacts />} />*/}
+              {/*<Route path='/invoices' element={<Invoices />} />*/}
+              {/*<Route path='/form' element={<Form />} />*/}
+              {/*<Route path='/bar' element={<Bar />} />*/}
+              {/*<Route path='/pie' element={<Pie />} />*/}
+              {/*<Route path='/line' element={<Line />} />*/}
+              {/*<Route path='/faq' element={<FAQ />} />*/}
+              {/*<Route path='/geography' element={<Geography />} />*/}
+              {/*<Route path='/calendar' element={<Calendar />} />*/}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
