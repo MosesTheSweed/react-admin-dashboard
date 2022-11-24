b66026331cbe55121c12c8a77efb79d7cb59e6ca
MosesTheSweed <MosesTheSweed@proton.me>
2022-11-24 14:29:54 -0500

    Have the stock calendar working
    Need to use extended properties to associate events to user??

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-import {ColorModeContext, useMode} from '/src/theme.js';
-import {CssBaseline, ThemeProvider} from '@mui/material';
-import {Routes, Route} from 'react-router-dom';
-import Topbar from '/src/scenes/global/Topbar.jsx';
-import Sidebar from '/src/scenes/global/Sidebar.jsx';
-// import Dashboard from '/src/scenes/dashboard.jsx';
-// import Team from '/src/scenes/team.jsx';
-// import Invoices from '/src/scenes/invoices.jsx';
-// import Contacts from '/src/scenes/contacts.jsx';
-// import Bar from '/src/scenes/bar.jsx';
-// import Form from '/src/scenes/form.jsx';
-// import Line from '/src/scenes/line.jsx';
-// import Pie from '/src/scenes/pie.jsx';
-// import FAQ from '/src/scenes/faw.jsx';
-// import Geography from '/src/scenes/geography.jsx';
-// import Calendar from '/src/scenes/calendar.jsx';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Calendar from "./scenes/calendar/calendar";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
-          <main className='content'>
-            <Topbar />
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              {/*<Route path='/' element={<Dashboard />} />*/}
-              {/*<Route path='/team' element={<Team />} />*/}
-              {/*<Route path='/contacts' element={<Contacts />} />*/}
-              {/*<Route path='/invoices' element={<Invoices />} />*/}
-              {/*<Route path='/form' element={<Form />} />*/}
-              {/*<Route path='/bar' element={<Bar />} />*/}
-              {/*<Route path='/pie' element={<Pie />} />*/}
-              {/*<Route path='/line' element={<Line />} />*/}
-              {/*<Route path='/faq' element={<FAQ />} />*/}
-              {/*<Route path='/geography' element={<Geography />} />*/}
-              {/*<Route path='/calendar' element={<Calendar />} />*/}
+              <Route path="/" element={<Dashboard />} />
+              {/*<Route path="/team" element={<Team />} />*/}
+              {/*<Route path="/contacts" element={<Contacts />} />*/}
+              {/*<Route path="/invoices" element={<Invoices />} />*/}
+              {/*<Route path="/form" element={<Form />} />*/}
+              {/*<Route path="/bar" element={<Bar />} />*/}
+              {/*<Route path="/pie" element={<Pie />} />*/}
+              {/*<Route path="/line" element={<Line />} />*/}
+              {/*<Route path="/faq" element={<FAQ />} />*/}
+              <Route path="/calendar" element={<Calendar />} />
+              {/*<Route path="/geography" element={<Geography />} />*/}
             </Routes>
           </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
